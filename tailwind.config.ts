c2fec3dd3c381d0e8db9567e5b0d82c4a9629764
Tomas Marques <ninbo.sum@pm.me>
2023-08-17 20:12:29 +0100

    style: extend color pallet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,33 +4,28 @@ import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/styles/**/*.{scss,sass,css}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{scss,sass,css}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'lgreen': '#dce9d4', // * light green
-      'mgreen': '#bef2ce', // * Mid green
-      'agreen': '#305c3d', // * Accent green
-      'lyellow': '#fae7b2', // * light yelllow
-      'ayellow': '#efb922', // * Accent yellow
-      'lblue': '#e7f1fe', // * Lhight yellow
-      'mblue': '#95c3ff', // * Mid yellow
-      'ablue': '#003a92', // * Accent yellow
-      'lred': '#efb922', // * Light yellow
-      'mred': '#efb922', // * Mid yellow
-      'ared': '#efb922', // * Accent yellow
-    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        lgreen: "#dce9d4", // * light green
+        mgreen: "#bef2ce", // * Mid green
+        agreen: "#305c3d", // * Accent green
+        lyellow: "#fae7b2", // * light yelllow
+        ayellow: "#efb922", // * Accent yellow
+        lblue: "#e7f1fe", // * Lhight yellow
+        mblue: "#95c3ff", // * Mid yellow
+        ablue: "#003a92", // * Accent yellow
+        lred: "#efb922", // * Light yellow
+        mred: "#efb922", // * Mid yellow
+        ared: "#efb922", // * Accent yellow
       },
     },
   },
