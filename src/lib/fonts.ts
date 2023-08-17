9f4638e7864a9722069179f157ac5205b006a028
Tomas Marques <ninbo.sum@pm.me>
2023-08-17 12:32:45 +0100

    build: NextUi setup, styles setup, proj structure

---
+++ src/lib/fonts.ts
@@ -0,0 +1,5 @@
+import { Comfortaa, League_Spartan, Nunito } from "next/font/google";
+
+export const Conteudo = Nunito({ subsets: ["latin"] });
+export const Display = Comfortaa({ subsets: ["latin"] });
+export const Titulo = League_Spartan({ subsets: ["latin"] });
