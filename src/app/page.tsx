499783b64746e811fb504b29a4be436f7aeb00df
Tomas Marques <ninbo.sum@pm.me>
2023-08-17 20:13:52 +0100

    feat: Exemplo de utilização do popup de questoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import StepTestLogo from "@/componentes/logos/StepTest";
+import { GridProvaQuestoes } from "@/componentes/popups/Grids";
+import { Button } from "@nextui-org/button";
+import { useDisclosure } from "@nextui-org/use-disclosure";
+
 export default function Home() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <main>
-      <h1>Hello world</h1>
+      <StepTestLogo />
+      <Button onPress={onOpen}>Open modal</Button>
+      <GridProvaQuestoes
+        state={{
+          onOpen,
+          isOpen,
+          onOpenChange,
+        }}
+        content={{
+          titulo: "Matematica#2",
+          num_questoes: 8,
+        }}
+      />
     </main>
   );
 }
