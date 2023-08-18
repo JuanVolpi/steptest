fac70e598a49e59d301688b51c03b98c63906e77
Tomas Marques <ninbo.sum@pm.me>
2023-08-18 14:20:49 +0100

    feat: exemplo card genérico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 "use client";
 
+import {
+  SimpleBlueButton,
+  SimpleRedButton,
+  SimpleYellowButton,
+} from "@/componentes/buttons/Buttons";
+import { GenericCard } from "@/componentes/cards/Card";
+import {
+  ChatBubbleSolid,
+  ListBullet,
+  LockOutline,
+  QuestionSolid,
+  Share,
+  StarOutline,
+  TrashBin,
+} from "@/componentes/icons/HeroIcons";
 import StepTestLogo from "@/componentes/logos/StepTest";
 import { GridProvaQuestoes } from "@/componentes/popups/Grids";
 import { Button } from "@nextui-org/button";
@@ -12,6 +27,72 @@ export default function Home() {
     <main>
       <StepTestLogo />
       <Button onPress={onOpen}>Open modal</Button>
+      <br />
+      <GenericCard
+        headerElements={[
+          <SimpleBlueButton
+            key={"fav"}
+            content={"Favorito"}
+            endIcon={<StarOutline fill="rgb(129 140 248)" />}
+          />,
+          <SimpleBlueButton
+            key={"share"}
+            content={"Partilhar"}
+            endIcon={<Share fill="rgb(129 140 248)" />}
+          />,
+          <SimpleBlueButton
+            key={"lock"}
+            content={"Trancar"}
+            endIcon={<LockOutline fill="rgb(129 140 248)" />}
+          />,
+          <SimpleRedButton
+            key={"lock"}
+            content={"Apagar"}
+            endIcon={<TrashBin fill="salmon" />}
+          />,
+        ]}
+        content={[
+          {
+            icon: <QuestionSolid fill="blue" />,
+            tittle: "Questões",
+            inner: (
+              <p>
+                Uma empresa fabrica 600 peças por dia e trabalha 5 dias por
+                semana. Quantas peças são fabricadas em um mês de 4 semanas?
+              </p>
+            ),
+          },
+          {
+            icon: <ChatBubbleSolid fill="blue" />,
+            tittle: "Respostas",
+            inner: (
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Commodi veritatis alias, quidem odit rem consequatur nisi.
+              </p>
+            ),
+          },
+          {
+            icon: <ListBullet fill="blue" />,
+            headSubs: (
+              <>
+                <SimpleYellowButton
+                  content={"Info"}
+                  endIcon={<QuestionSolid fill="orange" />}
+                />
+              </>
+            ),
+            tittle: "Respostas",
+            inner: (
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Commodi veritatis alias, quidem odit rem consequatur nisi.
+              </p>
+            ),
+          },
+        ]}
+      />
+      <br />
       <GridProvaQuestoes
         state={{
           onOpen,
