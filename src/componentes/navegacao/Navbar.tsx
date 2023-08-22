e4bad1058a041c3c3a96756538f0583e94fec4bd
Tomas Marques <ninbo.sum@pm.me>
2023-08-22 16:24:38 +0100

    fix: Navbar appearance

--- src/componentes/navegacao/Navbar.tsx
+++ src/componentes/navegacao/Navbar.tsx
@@ -1,13 +1,12 @@
 "use client";
-import React from "react";
-import StepTest from "../logos/StepTest";
 import { SimpleYellowButton } from "../buttons/Buttons";
+import StepTest from "../logos/StepTest";
 
 export default function BarraDeNavegacao() {
   return (
     <div className="w-full h-max flex flex-row bg-white">
       <StepTest></StepTest>
-      <div className=" flex  flex-row gap-5 w-full self-center justify-end pr-2">
+      <div className="flex flex-row gap-5 w-full self-center justify-end pr-2">
         <SimpleYellowButton content={"Perfil"}></SimpleYellowButton>
         <SimpleYellowButton content={"Provas"}></SimpleYellowButton>
         <SimpleYellowButton content={"Questões"}></SimpleYellowButton>
