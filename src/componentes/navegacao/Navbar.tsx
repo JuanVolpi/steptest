39625f337bacd4d0208456e216db292a07e6e2c0
Tomás Marques <ninbo.sum@pm.me>
2023-08-23 14:32:02 +0100

    Update Navbar.tsx - fix bad merge
    Navbar contente lost, now recovered

--- src/componentes/navegacao/Navbar.tsx
+++ src/componentes/navegacao/Navbar.tsx
@@ -1,17 +1,42 @@
 "use client";
-import { SimpleYellowButton } from "../buttons/Buttons";
+import React from "react";
+
 import StepTest from "../logos/StepTest";
+import { SimpleYellowButton, SimpleYellowButtonLink } from "../buttons/Buttons";
+import {
+  DocumentText,
+  QuestionSolid,
+  UserGroup,
+  User,
+  ChartPie,
+} from "../icons/HeroIcons";
 
 export default function BarraDeNavegacao() {
   return (
     <div className="w-full h-max flex flex-row bg-white">
       <StepTest></StepTest>
-      <div className="flex flex-row gap-5 w-full self-center justify-end pr-2">
-        <SimpleYellowButton content={"Perfil"}></SimpleYellowButton>
-        <SimpleYellowButton content={"Provas"}></SimpleYellowButton>
-        <SimpleYellowButton content={"Questões"}></SimpleYellowButton>
-        <SimpleYellowButton content={"Minha Turma"}></SimpleYellowButton>
-        <SimpleYellowButton content={"Dashboard"}></SimpleYellowButton>
+      <div className=" flex  flex-row gap-5 w-full self-center justify-end pr-2">
+        <SimpleYellowButton
+          content={"Perfil"}
+          endIcon={<User fill="rgb(195, 131, 29)" />}
+        ></SimpleYellowButton>
+        <SimpleYellowButtonLink
+          content={"Provas"}
+          endIcon={<DocumentText fill="rgb(195, 131, 29)" />}
+          href={"/provas"}
+        ></SimpleYellowButtonLink>
+        <SimpleYellowButton
+          content={"Questões"}
+          endIcon={<QuestionSolid fill="rgb(195, 131, 29)" />}
+        ></SimpleYellowButton>
+        <SimpleYellowButton
+          content={"Minha Turma"}
+          endIcon={<UserGroup fill="rgb(195, 131, 29)" />}
+        ></SimpleYellowButton>
+        <SimpleYellowButton
+          content={"Dashboard"}
+          endIcon={<ChartPie fill="rgb(195, 131, 29)" />}
+        ></SimpleYellowButton>
       </div>
     </div>
   );
