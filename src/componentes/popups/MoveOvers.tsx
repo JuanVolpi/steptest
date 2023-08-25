2fd2291819b50d135cbc47861931c9d55df7485a
Tomas Marques <ninbo.sum@pm.me>
2023-08-25 03:15:57 +0100

    feat: Nav side Bar detalhes questao

--- src/componentes/popups/MoveOvers.tsx
+++ src/componentes/popups/MoveOvers.tsx
@@ -1,7 +1,6 @@
 import {
   Card,
   CardBody,
-  CardFooter,
   CardHeader,
   Chip,
   Divider,
@@ -10,6 +9,7 @@ import {
 
 import {
   ChevronDownIcon,
+  ChevronRightIcon,
   ChevronUpIcon,
   DocumentTextIcon,
   EyeIcon,
@@ -17,8 +17,13 @@ import {
   UserCircleIcon,
 } from "@heroicons/react/20/solid";
 
-import { DetalhesQuestaoProps } from "@/lib/types/componentes/move_overs";
+import {
+  DetalhesQuestaoPageSelectorProps,
+  DetalhesQuestaoProps,
+  NavegacaoDetalhesQuestao,
+} from "@/lib/types/componentes/move_overs";
 import "@/styles/component/popups/MoveOvers.scss";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export function DetalhesQuestao(props: DetalhesQuestaoProps) {
   return (
@@ -32,8 +37,8 @@ export function DetalhesQuestao(props: DetalhesQuestaoProps) {
           <Divider orientation="vertical" className="h-6" />
           <User
             lang="pt-br"
-            name="Camila Alves"
-            description="Autora / Matemática"
+            name={props.autor}
+            description="Autora"
             avatarProps={{
               src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
               isBordered: true,
@@ -52,7 +57,7 @@ export function DetalhesQuestao(props: DetalhesQuestaoProps) {
               className="px-2"
               startContent={<DocumentTextIcon className="w-5 h-5" />}
             >
-              Testes <strong>{props.quanTestes}</strong>
+              Testes <strong>{props.numTestes}</strong>
             </Chip>
             <Chip
               variant="flat"
@@ -76,9 +81,138 @@ export function DetalhesQuestao(props: DetalhesQuestaoProps) {
         </header>
       </CardHeader>
       <Divider />
-      <CardBody className="body"></CardBody>
-      <Divider />
-      <CardFooter className="footer"></CardFooter>
+      <CardBody className="body">
+        <DetalhesQuestaoPageSelector
+          defaultSelect={"notas"}
+          onSelectionChange={function (key: string): void {}}
+          sections={defaultNavigation}
+        />
+        <section className="info"></section>
+      </CardBody>
+      {/* <Divider />
+      <CardFooter className="footer"></CardFooter> */}
     </Card>
+  );
+}
+
+const defaultNavigation: NavegacaoDetalhesQuestao[] = [
+  {
+    titulo: "Especificações",
+    paginas: [
+      {
+        nome: "BNCC",
+        desc: "Requisitos e objetivos",
+        key: "bncc",
+      },
+      {
+        nome: "Publicação",
+        desc: "Visibilidade e utilização",
+        key: "publicacao",
+      },
+    ],
+  },
+  {
+    titulo: "Multimédia",
+    paginas: [
+      {
+        nome: "Imagens",
+        desc: "Recursos visuais",
+        key: "imgs",
+      },
+    ],
+  },
+  {
+    titulo: "Conteúdo",
+    paginas: [
+      {
+        nome: "Respostas",
+        desc: "Opções de resposta",
+        key: "resps",
+      },
+      {
+        nome: "Explicações",
+        desc: "Explicação alternativas",
+        key: "explicacoes",
+      },
+    ],
+  },
+  {
+    titulo: "Anotações",
+    paginas: [
+      {
+        nome: "Comentários",
+        desc: "Feedback comunidade",
+        key: "feedback",
+      },
+      {
+        nome: "Notas",
+        desc: "Comentários do criador",
+        key: "notas",
+      },
+    ],
+  },
+];
+
+export function DetalhesQuestaoPageSelector(
+  props: DetalhesQuestaoPageSelectorProps,
+) {
+  const [selected, setSelected] = useState<string>(props.defaultSelect);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+  const rootParentScroll = useRef<HTMLElement>(null);
+
+  const allPages = useCallback(
+    () => props.sections.map((x) => x.paginas).flat(),
+    [props],
+  )();
+  const refs = useRef<HTMLElement[]>(new Array(allPages.length));
+
+  const scroll = useCallback(() => {
+    const currentElement = refs.current[selectedIndex as number];
+    if (rootParentScroll.current !== null) {
+      let selectedPageOffset = currentElement.offsetTop;
+      rootParentScroll.current.scrollTop =
+        selectedPageOffset - currentElement.clientHeight;
+    }
+  }, [selectedIndex, refs]);
+
+  useEffect(() => scroll(), [scroll]);
+
+  return (
+    <section className="pages" ref={rootParentScroll}>
+      {props.sections.map((sec, index) => (
+        <div key={index} className="seccao">
+          <h2>{sec.titulo}</h2>
+          <section>
+            {sec.paginas.map((pagina, indexSect) => {
+              const currentPageFlatIndex = allPages.indexOf(pagina);
+              return (
+                <div
+                  id={currentPageFlatIndex.toString()}
+                  key={indexSect}
+                  className={`page ${
+                    selected === pagina.key ? "selected" : ""
+                  }`}
+                  onClick={(_) => {
+                    setSelected(pagina.key);
+                    setSelectedIndex(currentPageFlatIndex);
+                  }}
+                  ref={(el) =>
+                    (refs.current[currentPageFlatIndex] = el as HTMLElement)
+                  }
+                >
+                  <div>
+                    <h3>{pagina.nome}</h3>
+                    <span>{pagina.desc}</span>
+                  </div>
+                  <ChevronRightIcon className="endContent" />
+                </div>
+              );
+            })}
+          </section>
+          {index < props.sections.length - 1 ? <Divider /> : null}
+        </div>
+      ))}
+    </section>
   );
 }
--- src/lib/types/componentes/move_overs.ts
+++ src/lib/types/componentes/move_overs.ts
@@ -1,6 +1,22 @@
 export type DetalhesQuestaoProps = {
   nomeQuestao: string;
-  quanTestes: number;
+  numTestes: number;
   visibilidade: "Publica" | "Privada" | "Rede";
   posReacts: number;
+  autor: string;
+};
+
+export type NavegacaoDetalhesQuestao = {
+  titulo: string;
+  paginas: {
+    nome: string;
+    desc: string;
+    key: string;
+  }[];
+};
+
+export type DetalhesQuestaoPageSelectorProps = {
+  defaultSelect: string;
+  onSelectionChange: (key: string) => void;
+  sections: NavegacaoDetalhesQuestao[];
 };
