fb21d417d7005c76a6881a0d59f37476ff09be4d
Juan Volpi <juan8volpi@gmail.com>
2023-09-06 16:45:08 +0100

    feat: página de dashboards

--- src/app/dashboards/page.tsx
+++ src/app/dashboards/page.tsx
@@ -1,22 +1,14 @@
-"use client";
-
+import ResultsTable from "@/componentes/cards/ResultsTable";
+import { alunos, respostas } from "@/lib/test_data/dados";
 import { Divider } from "@nextui-org/react";
+
+ // Certifique-se de importar os dados corretos
 
 export default function Dashboards() {
   return (
     <>
-      <main className="w-full h-full bg-white rounded p-3 px-4 grid grid-cols-[25%_auto] grid-rows-1 gap-4">
-        {/* Selecionador turma, materia e dados */}
-        <section className="w-full h-full">
-          <fieldset className="border border-slate-300 p-2 px-4 rounded">
-            <legend className="font-bold text-xl text-blue-500 bg-sky-100 p-1 px-2 rounded-md">
-              Turma
-            </legend>
-            <Divider />
-          </fieldset>
-        </section>
-        {/* Representação dos dados */}
-        <section className="w-full h-full border rounded"> </section>
+      <main className="w-full h-full bg-white rounded p-3 px-4">
+        <ResultsTable alunos={alunos} respostas={respostas} />
       </main>
     </>
   );
