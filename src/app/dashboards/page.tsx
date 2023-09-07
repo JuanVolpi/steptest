b1b07e2279cc6940f18cf66db8874cc79605a2cd
Tomas Marques <ninbo.sum@pm.me>
2023-09-07 16:23:57 +0100

    feat: styles, and test data feats

--- src/app/dashboards/page.tsx
+++ src/app/dashboards/page.tsx
@@ -35,8 +35,11 @@ export default function Dashboards() {
 
   const [filterText, setFilterText] = useState<string>("");
   const [showAutoComplete, setShowAutoComplete] = useState(false);
+  const [provaSelecionada, setProvaSelecionada] = useState("");
+
+  type Dashboard = "Vista Geral" | "Aluno-Aluno" | "Questao-Questao";
   const [selectedDashboard, setSelectedDashboard] = useState<
-    string | undefined
+    Dashboard | undefined
   >();
 
   const seccoesObj: { [key: string]: HTMLDivElement } = useMemo(() => {
@@ -70,10 +73,23 @@ export default function Dashboards() {
       (selectedPageOffset - currentElement.clientHeight) * 1.4;
   }
 
+  function handleDashboardSelect(): React.JSX.Element {
+    switch (selectedDashboard) {
+      case undefined:
+        return <></>;
+      case "Aluno-Aluno":
+        return <></>;
+      case "Questao-Questao":
+        return <></>;
+      case "Vista Geral":
+        return <TabelaAlunos alunos={alunos} respostas={respostas} />;
+    }
+  }
+
   return (
     <main className="inline-flex gap-4 pb-5">
       {/* Selecionador turma, materia e dados */}
-      <section className="max-w-[30%] pb-6 bg-white p-4 px-6 rounded">
+      <section className="max-w-[40%]  bg-white p-4 pb-2 rounded max-h-fit">
         <h2 className="font-bold text-xl text-blue-500 bg-sky-100 p-2 rounded-md w-fit before:content-['#'] before:mr-1 before:text-blue-300 hover:before:text-blue-500 before:ease-soft-spring before:duration-250">
           Provas
         </h2>
@@ -115,41 +131,52 @@ export default function Dashboards() {
               />
             </section>
           </header>
-          <main className="max-h-[450px] overflow-y-scroll space-y-4 snap-y scroll-smooth shadow-inner">
+          {/* Zona Lista Provas */}
+          <main className="max-h-[72vh] overflow-y-scroll space-y-4 snap-y scroll-smooth shadow-inner">
             {seccoes.map(({ nome, numProvas }, i) => (
               <div
                 key={i}
                 ref={(e) => {
                   refs.current[nome] = e as HTMLDivElement;
                 }}
-                className={`rounded-md py-2 ${
+                className={`rounded-md py-2.5 ${
                   filterText === nome ? "bg-fuchsia-100/50" : "bg-inherit"
                 }`}
               >
-                <SeccaoProvas quantProvas={numProvas} tituloSeccao={nome} />
+                <SeccaoProvas
+                  quantProvas={numProvas}
+                  tituloSeccao={nome}
+                  onProvaSelect={(prova) => {
+                    setProvaSelecionada(prova);
+                  }}
+                />
               </div>
             ))}
           </main>
         </section>
 
         <Divider className="bg-slate-200" />
-
-        <section className="mt-4 space-y-4">
-          <header className="space-y-2">
+      </section>
+      {/* Representação dos dados */}
+      <section>
+        {/* Zona Dashboards */}
+        <section className="space-y-4 p-4 bg-white rounded-md mb-5">
+          <header className="inline-flex gap-3 items-baseline">
             <h2 className="font-bold text-xl text-blue-500 bg-sky-100 p-2 rounded-md w-fit before:content-['#'] before:mr-1 before:text-blue-300 hover:before:text-blue-500 before:ease-soft-spring before:duration-250">
-              Dashboard
+              Dashboards
             </h2>
             <i className="text-default-400 text-sm">
               Selecione o tipo de dashboard a visualizar
             </i>
           </header>
-          <main className="flex flex-col gap-0.5 justify-center">
+          {/* Detalhes Dashboard */}
+          <main className="flex flex-col gap-0.5 justify-start max-w-fit">
             <AnimatePresence>
               {selectedDashboard !== undefined && (
                 <motion.div
                   initial={{ opacity: 0, x: "-20px" }}
                   animate={{ opacity: 1, x: "0px" }}
-                  exit={{ opacity: 0, y: "20px", animationDelay: "0.5s" }}
+                  exit={{ opacity: 0, y: "20px", animationDelay: "0.2s" }}
                   transition={{ ease: "easeInOut", duration: 0.35 }}
                   className="inline-flex gap-2 my-2 items-center"
                 >
@@ -163,7 +190,7 @@ export default function Dashboards() {
                     transition={{
                       ease: "easeInOut",
                       duration: 0.25,
-                      delay: 0.5,
+                      delay: 0.2,
                     }}
                     className="inline-flex items-center gap-0.5"
                   >
@@ -185,7 +212,7 @@ export default function Dashboards() {
               color="secondary"
               variant="underlined"
               classNames={{
-                base: "rounded-md border border-fuchsia-400 mb-1 py-1 shadow mx-auto w-fit",
+                base: "rounded-md border border-fuchsia-400 mb-1 py-1 shadow w-fit",
               }}
             >
               <Tab key="Provas" title="Específicos para prova">
@@ -194,7 +221,7 @@ export default function Dashboards() {
                   animate={{ opacity: 1, y: "0px" }}
                   exit={{ opacity: 0, y: "-15px" }}
                   transition={{ ease: "easeInOut", duration: 0.35 }}
-                  className="flex flex-row flex-wrap items-center justify-center gap-2"
+                  className="flex flex-row flex-wrap items-center justify-start gap-2"
                 >
                   <Button
                     variant="flat"
@@ -212,7 +239,7 @@ export default function Dashboards() {
                     startContent={
                       <QuestionMarkCircleIcon className="w-5 h-5" />
                     }
-                    onClick={() => setSelectedDashboard("Questão-A-Questão")}
+                    onClick={() => setSelectedDashboard("Questao-Questao")}
                   >
                     Questão-A-Questão
                   </Button>
@@ -240,25 +267,42 @@ export default function Dashboards() {
             </Tabs>
           </main>
         </section>
+        <AnimatePresence>
+          <motion.section
+            initial={{ opacity: 0, x: "200%" }}
+            animate={{ opacity: 1, x: "-0%" }}
+            exit={{ opacity: 0, x: "200%" }}
+            transition={{ ease: "easeInOut", duration: 0.7 }}
+          >
+            <AnimatePresence>
+              {selectedDashboard !== undefined && (
+                <motion.div
+                  initial={{ x: "100%" }}
+                  animate={{ x: "-0%" }}
+                  exit={{ x: "100%" }}
+                  transition={{ ease: "easeInOut", duration: 0.7 }}
+                  className="w-full h-fit aspect-video rounded bg-white p-4 grid place-items-start"
+                >
+                  <h2 className="text-lg font-bold text-fuchsia-600 p-2 px-3 bg-fuchsia-100 rounded-md">
+                    Prova: {provaSelecionada}
+                  </h2>
+                  <Spacer y={3} />
+                  <Divider />
+                  <Spacer y={3} />
+                  <motion.section
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ ease: "easeInOut", duration: 0.7 }}
+                  >
+                    {handleDashboardSelect()}
+                  </motion.section>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </motion.section>
+        </AnimatePresence>
       </section>
-      {/* Representação dos dados */}
-      <AnimatePresence>
-        <motion.section
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ ease: "easeInOut", duration: 0.35 }}
-          className="w-full h-fit aspect-video rounded bg-white p-4 grid place-items-start"
-        >
-          <h2 className="text-lg font-bold text-fuchsia-600 p-2 px-3 bg-fuchsia-100 rounded-md">
-            Prova: Nome Prova
-          </h2>
-          <Spacer y={3} />
-          <Divider />
-          <Spacer y={3} />
-          <TabelaAlunos alunos={alunos} respostas={respostas} />
-        </motion.section>
-      </AnimatePresence>
     </main>
   );
 }
@@ -331,9 +375,15 @@ function TextInputAutoCompleteOptions(
 export type SeccaoProvasProps = {
   tituloSeccao: string;
   quantProvas: number;
+  onProvaSelect: (prova: string) => void;
 };
 
 function SeccaoProvas(props: SeccaoProvasProps) {
+  const [selecionada, setSelecionada] = useState("");
+  function nomeProva(i: number): string {
+    return `${props.tituloSeccao} ${i}`;
+  }
+
   return (
     <section className="max-h-[400px] overflow-y-scroll snap-start ">
       <div className="mb-2.5 sticky top-0 w-fit flex gap-3 items-end ">
@@ -346,31 +396,56 @@ function SeccaoProvas(props: SeccaoProvasProps) {
           <DocumentTextIcon className="w-6 h-6" />
         </div>
       </div>
-      <ul className="flex flex-row flex-wrap items-center justify-start gap-3 scroll-smooth snap-y ml-1.5 pr-1 pb-2 max-w-fit">
-        {new Array(props.quantProvas).fill(1).map((_, i) => (
-          <div
-            key={i}
-            className="flex flex-col gap-3 items-center justify-evenly border-1.5 w-fit px-4 py-2.5 rounded border-fuchsia-300/50 bg-white snap-center ease-soft-spring duration-200 transition-all hover:border-fuchsia-300 hover:scale-105 hover:cursor-pointer"
-          >
-            <header>
-              <h4 className="font-semibold">Nome Prova</h4>
-              <p className="font-thin text-default-500 text-xs">12/03/2022</p>
-            </header>
-            <main className="flex flex-row items-center gap-2 text-sm">
-              <p
-                className={`font-bold ${
-                  i % 2 === 0
-                    ? "text-green-600 bg-green-600/10"
-                    : "text-orange-500 bg-orange-500/10"
-                } px-3 py-0.5  rounded-md`}
-              >
-                {i % 2 === 0 ? "Corrigida" : "Corrigir"}
-              </p>
-              {/* <ArrowRightCircleIcon className="w-7 h-7 text-fuchsia-500" /> */}
-            </main>
-          </div>
-        ))}
-      </ul>
+      <AnimatePresence>
+        <motion.ul className="grid grid-cols-3 lg:grid-cols-1 xl:grid-cols-3 items-center justify-between gap-1.5 scroll-smooth snap-y  pb-2.5 w-full">
+          {new Array(props.quantProvas).fill(1).map((_, i) => (
+            <motion.li
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0 }}
+              transition={{
+                ease: "circInOut",
+                duration: 0.35,
+                delay: Number(`0.${i + 1}`),
+              }}
+              key={i}
+              className="w-full flex flex-col gap-4 items-center justify-evenly border-1.5 px-3 py-2 rounded border-fuchsia-300/50 bg-white snap-center ease-soft-spring duration-200 transition-all hover:border-fuchsia-300 hover:scale-105 hover:cursor-pointer"
+              style={{
+                border:
+                  selecionada === nomeProva(i) ? "solid 2px blue" : undefined,
+              }}
+              onClick={() => {
+                if (selecionada !== nomeProva(i)) {
+                  setSelecionada(nomeProva(i));
+                  props.onProvaSelect(nomeProva(i));
+                } else {
+                  setSelecionada("");
+                  props.onProvaSelect("");
+                }
+              }}
+            >
+              <header>
+                <h4 className="font-semibold">{nomeProva(i)}</h4>
+                <p className="font-thin text-default-500 text-xs text-center mt-0.5">
+                  12/03/2022
+                </p>
+              </header>
+              <main className="flex flex-row items-center gap-2 text-sm">
+                <p
+                  className={`font-bold ${
+                    i % 2 === 0
+                      ? "text-green-600 bg-green-600/10"
+                      : "text-orange-500 bg-orange-500/10"
+                  } px-3 py-0.5  rounded-md`}
+                >
+                  {i % 2 === 0 ? "Corrigida" : "Corrigir"}
+                </p>
+                {/* <ArrowRightCircleIcon className="w-7 h-7 text-fuchsia-500" /> */}
+              </main>
+            </motion.li>
+          ))}
+        </motion.ul>
+      </AnimatePresence>
     </section>
   );
 }
