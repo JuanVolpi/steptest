54b160ebf8d19d9ccb87c500b36763536439883b
Juan Volpi <juan8volpi@gmail.com>
2023-09-13 17:37:02 +0100

    refactor: página criar questões

--- src/app/questoes/criar/page.tsx
+++ src/app/questoes/criar/page.tsx
@@ -1,273 +1,287 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
-import CustomDropdown from "@/components/navigation/CustomDropdown";
+import { SmallDeleteButton, SmallSelectAllButton } from "@/components/buttons";
 
-import { PageTag } from "@/components/cards/Tags";
-import ConfirmationModal from "@/components/popups/Modal";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Contexto from "@/components/inputs/Contexto";
+import { Conteudo } from "@/lib/fonts";
 import {
-  Button,
-  ButtonGroup,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownTrigger,
-  useDisclosure,
-} from "@nextui-org/react";
-import React, { useState } from "react";
-import { Comentario, Enunciado } from "./ComponentsCaller";
+  ChatBubbleBottomCenterTextIcon,
+  CheckCircleIcon,
+  QuestionMarkCircleIcon,
+  TrashIcon,
+} from "@heroicons/react/20/solid";
+import { Button } from "@nextui-org/button";
+import { CheckboxGroup, Checkbox } from "@nextui-org/checkbox";
+import { Divider } from "@nextui-org/divider";
+import { Tooltip } from "@nextui-org/react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
+import { useImmerReducer } from "use-immer";
 
 export default function app() {
-  const [selectedKeys1, setSelectedKeys1] = useState(new Set([""]));
-  const [selectedKeys2, setSelectedKeys2] = useState(new Set([""]));
-  const [selectedKeys3, setSelectedKeys3] = useState(new Set([""]));
-  const [selectedKeys4, setSelectedKeys4] = useState(new Set([""]));
-  const [selectedKeys5, setSelectedKeys5] = useState(new Set([""]));
-  const [selectedKeys6, setSelectedKeys6] = useState(new Set([""]));
-  const [selectedKeys7, setSelectedKeys7] = useState(new Set([""]));
+  /* TODOs
+    - Filtrozao
 
-  const handleDropdown1SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys1(newSelectedKeys);
-  };
-  const handleDropdown2SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys2(newSelectedKeys);
-  };
-  const handleDropdown3SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys3(newSelectedKeys);
-  };
-  const handleDropdown4SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys4(newSelectedKeys);
-  };
-  const handleDropdown5SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys5(newSelectedKeys);
-  };
-  const handleDropdown6SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys6(newSelectedKeys);
-  };
-  const handleDropdown7SelectionChange = (newSelectedKeys: Set<string>) => {
-    setSelectedKeys7(newSelectedKeys);
+    Zona professor & aluno
+    
+    Linha de cima: Drop-down
+      Ano, area, codigo
+  */
+
+  /* 
+    [_] Dispatch: Action (type) + Value
+    [_] Reducer: (action ?) -> ok... action.x => abcd(value)
+    [X] State: obj
+  */
+
+  type Action = {
+    type: "toggle" | "select all" | "deselect all";
+    seccao: "questao" | "identificador" | "extra";
+    value?: string[];
   };
 
-  const dropdownOptions1 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-  const dropdownOptions2 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-  const dropdownOptions3 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-  const dropdownOptions4 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-  const dropdownOptions5 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-  const dropdownOptions6 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-  const dropdownOptions7 = [
-    { key: "option1", label: "Option 1" },
-    { key: "option2", label: "Option 2" },
-    { key: "option3", label: "Option 3" },
-  ];
-
-  const [selectedOption, setSelectedOption] = React.useState("enunciado");
-
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const descriptionsMap = {
-    enunciado: "Adiciona um campo para o enunciado da questão",
-    comentario: "Adiciona um campo para o comentário da questão",
-    rebase: "Ay.",
-  };
-
-  const labelsMap: { [key: string]: string } = {
-    enunciado: "Adicionar Enunciado",
-    comentario: "Adicionar Comentário",
-    rebase: "y",
-  };
-
-  const [exibirEnunciado, setExibirEnunciado] = useState(false);
-  const [exibirComentario, setExibirComentario] = useState(false);
-  const [title, setTitle] = useState("");
-
-  const selectedOptionValue = Array.from(selectedOption)[0];
-
-  const handleButtonClick = () => {
-    switch (selectedOption) {
-      case "enunciado":
-        setExibirEnunciado(true);
-        break;
-      case "comentario":
-        setExibirComentario(true);
-        break;
+  function reducer(draft: NewType, action: Action) {
+    switch (action.type) {
+      case "toggle":
+        return void (draft[action.seccao] = action.value as string[]);
+      case "deselect all":
+        return void (draft[action.seccao] = baseState[action.seccao].filter(
+          (x) => x === "questao"
+        ));
+      case "select all":
+        return void (draft[action.seccao] = baseState[action.seccao]);
     }
+  }
+
+  type NewType = {
+    questao: string[];
+    identificador: string[];
+    extra: string[];
   };
+
+  const baseState: NewType = {
+    questao: ["questao", "contexto", "imgs"],
+    identificador: [],
+    extra: [],
+  };
+
+  const [state, dispatch] = useImmerReducer<NewType, Action>(reducer, {
+    questao: ["questao"],
+    identificador: [],
+    extra: [],
+  });
 
   return (
-    <div className="overflow-hidden">
-      <header className="bg-bglgreen w-full h-fit px-4 pb-3 ">
-        <div className="w-full justify-between h-max flex flex-row">
-          <h1 className="font-confortaa text-[40px] text-ayellow w-fit">
-            Questões - Criar
-          </h1>
-          <div className="self-center">
-            <PageTag content={"Questão"}></PageTag>
-          </div>
-        </div>
-        <div className="w-full h-2 pb-3 rounded bg-mgreen self-center"></div>
-      </header>
-      <div className="bg-bglgreen w-full h-[87%] px-4 overflow-y-scroll">
-        <section className="grid grid-cols-6 gap-5">
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys1}
-            onSelectChange={handleDropdown1SelectionChange}
-            options={dropdownOptions1}
-            title={"Área de conhecimento"}
-          ></CustomDropdown>
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys2}
-            onSelectChange={handleDropdown2SelectionChange}
-            options={dropdownOptions2}
-            title={"Nível de dificuldade"}
-          ></CustomDropdown>
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys3}
-            onSelectChange={handleDropdown3SelectionChange}
-            options={dropdownOptions3}
-            title={"Ano"}
-          ></CustomDropdown>
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys4}
-            onSelectChange={handleDropdown4SelectionChange}
-            options={dropdownOptions4}
-            title={"Código"}
-          ></CustomDropdown>
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys5}
-            onSelectChange={handleDropdown5SelectionChange}
-            options={dropdownOptions5}
-            title={"Ordem"}
-          ></CustomDropdown>
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys6}
-            onSelectChange={handleDropdown6SelectionChange}
-            options={dropdownOptions6}
-            title={"Descritor"}
-          ></CustomDropdown>
-        </section>
-        <div className="py-4">
-          <CustomDropdown
-            initialSelectedKeys={selectedKeys7}
-            onSelectChange={handleDropdown7SelectionChange}
-            options={dropdownOptions1}
-            title={"Descritor"}
-          ></CustomDropdown>
-        </div>
-        <main>
-          {exibirEnunciado && (
-            <Enunciado
-              click={() => {
-                setTitle("Enunciado");
-                onOpen();
-              }}
-              title={"Enunciado"}
-            />
-          )}
-          {exibirComentario && (
-            <Comentario
-              click={() => {
-                setTitle("Comentário");
-                onOpen();
-              }}
-              title={"Comentário"}
-            />
-          )}
-
-          <ConfirmationModal
-            onOpen={function (): void {
-              onOpen();
-            }}
-            onClose={function (): void {
-              onClose();
-            }}
-            isOpen={isOpen}
-            title={title}
-            click={() => {
-              switch (title) {
-                case "Comentário":
-                  setExibirComentario(false);
-                  break;
-                case "Enunciado":
-                  setExibirEnunciado(false);
-                  break;
-              }
-            }}
-          />
-        </main>
-        <ButtonGroup variant="flat">
-          <Button onClick={handleButtonClick}>
-            {labelsMap[selectedOptionValue]}
-          </Button>
-          <Dropdown placement="bottom-end">
-            <DropdownTrigger>
-              <Button isIconOnly>
-                <ChevronDownIcon />
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-              disallowEmptySelection
-              aria-label="Merge options"
-              selectedKeys={selectedOption}
-              selectionMode="single"
-              onSelectionChange={(x) => {
-                setSelectedOption(x as string);
-              }}
-              className="max-w-[300px]"
+    <div className="grid grid-cols-[1fr_3fr] gap-5 grid-rows-1 h-full w-full ">
+      <section className="h-full w-full rounded-md bg-white p-3 px-4 overflow-y-scroll">
+        <header className="space-y-1.5">
+          <h2 className="text-lg font-bold tracking-tight p-2 px-3 text-blue-500 bg-blue-300/20 rounded-md">
+            Campos
+          </h2>
+          <h4 className="text-sm text-slate-900/40 p-2 bg-slate-200/30 rounded">
+            Adicione e remova campos para a criação da questão.
+          </h4>
+        </header>
+        <div className="p-3" />
+        <AnimatePresence>
+          <main className="space-y-5">
+            <motion.section
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ ease: "easeInOut", duration: 0.7 }}
+              className="p-6 pt-4 border border-green-400 rounded-md shadow"
             >
-              <DropdownItem
-                key="enunciado"
-                description={descriptionsMap["enunciado"]}
-              >
-                {labelsMap["enunciado"]}
-              </DropdownItem>
-              <DropdownItem
-                key="comentario"
-                description={descriptionsMap["comentario"]}
-              >
-                {labelsMap["squash"]}
-              </DropdownItem>
-              <DropdownItem
-                key="rebase"
-                description={descriptionsMap["rebase"]}
-              >
-                {labelsMap["rebase"]}
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </ButtonGroup>
-        <button
-          onClick={() => {
-            setExibirEnunciado(true), setExibirComentario(true);
-          }}
+              <header className="flex flex-row items-center justify-between gap-4 w-full">
+                <h3 className="text-lg font-bold tracking-tight w-fit bg-green-100 text-green-600 p-2 py-1 rounded-md">
+                  Questão
+                </h3>
+                <div className="space-x-2">
+                  <SmallDeleteButton
+                    click={() =>
+                      dispatch({
+                        type: "deselect all",
+                        seccao: "questao",
+                      })
+                    }
+                  />
+                  <SmallSelectAllButton
+                    click={() => {
+                      dispatch({
+                        type: "select all",
+                        seccao: "questao",
+                        value: ["questao", "contexto"],
+                      });
+                    }}
+                  />
+                </div>
+              </header>
+              <div className="p-2.5" />
+              <div>
+                <h3 className="text-md font-semibold tracking-tight">
+                  Conteúdo Textual
+                </h3>
+                <CheckboxGroup
+                  label="Texto apresentado na questao"
+                  defaultValue={state.questao}
+                  classNames={{
+                    label: "text-sm",
+                    wrapper: "pt-0.5 px-2",
+                  }}
+                  value={state.questao}
+                  onValueChange={(value) =>
+                    dispatch({
+                      type: "toggle",
+                      seccao: "questao",
+                      value: value,
+                    })
+                  }
+                >
+                  <Checkbox value="contexto" style={Conteudo.style}>
+                    Contexto
+                  </Checkbox>
+                  <Checkbox
+                    value="questao"
+                    style={Conteudo.style}
+                    isRequired
+                    isSelected
+                    isDisabled
+                    classNames={{
+                      base: "opacity-100",
+                    }}
+                  >
+                    Questao
+                  </Checkbox>
+                  <div className="p-0.5" />
+                  <Divider />
+                  <div className="p-0.5" />
+                  <section>
+                    <h3 className="text-md font-semibold tracking-tight">
+                      Conteudo Multimedia
+                    </h3>
+                    <h5 className="text-sm tracking-tight text-slate-600/90">
+                      Conteudo Multimedia
+                    </h5>
+                    <div className="p-1" />
+                    <Checkbox value="imgs" style={Conteudo.style}>
+                      Imagens
+                    </Checkbox>
+                  </section>
+                </CheckboxGroup>
+              </div>
+            </motion.section>
+            <motion.section
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ ease: "easeInOut", duration: 0.7 }}
+              className="p-6 pt-4 border border-blue-400 rounded-md shadow"
+            >
+              <header className="flex flex-row items-center justify-between gap-4 w-full">
+                <h3 className="text-lg font-bold tracking-tight w-fit bg-blue-100 text-blue-600 p-2 py-1 rounded-md">
+                  Identificadores
+                </h3>
+                <div className="space-x-2">
+                  <SmallDeleteButton click={() => {}} />
+                  <SmallSelectAllButton click={() => {}} />
+                </div>
+              </header>
+              <div className="p-2.5" />
+              <div>
+                <h3 className="text-md font-semibold tracking-tight">
+                  Contúdos Adcionais
+                </h3>
+                <CheckboxGroup
+                  label="Informações atribuidas à questão "
+                  defaultValue={["questao"]}
+                  classNames={{
+                    label: "text-sm",
+                    wrapper: "pt-0.5 px-2",
+                  }}
+                >
+                  <Checkbox value="area-de-conhecimento">
+                    Área de conhecimento
+                  </Checkbox>
+                  <Checkbox value="nivel-de-dificuldade">
+                    Nível de dificuldade
+                  </Checkbox>
+                  <Checkbox value="ano">Ano</Checkbox>
+                  <Checkbox value="codigo">Código</Checkbox>
+                  <Checkbox value="ordem">Ordem</Checkbox>
+                  <Checkbox value="descritor">Descritor</Checkbox>
+                </CheckboxGroup>
+              </div>
+            </motion.section>
+            <motion.section
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ ease: "easeInOut", duration: 0.7 }}
+              className="p-6 pt-4 border border-fuchsia-400 rounded-md shadow"
+            >
+              <header className="flex flex-row items-center justify-between gap-4 w-full">
+                <h3 className="text-lg font-bold tracking-tight w-fit bg-fuchsia-100 text-fuchsia-600 p-2 py-1 rounded-md">
+                  Informação
+                </h3>
+                <div className="space-x-2">
+                  <SmallDeleteButton click={() => {}} />
+                  <SmallSelectAllButton click={() => {}} />
+                </div>
+              </header>
+              <div className="p-2.5" />
+              <div>
+                <h3 className="text-md font-semibold tracking-tight">
+                  Conteúdo Informacional
+                </h3>
+                <CheckboxGroup
+                  label="Conteúdos adicionais para os educadores"
+                  defaultValue={["questao"]}
+                  classNames={{
+                    label: "text-sm",
+                    wrapper: "pt-0.5 px-2",
+                  }}
+                >
+                  <Checkbox value="area-de-conhecimento">
+                    Área de conhecimento
+                  </Checkbox>
+                  <Checkbox value="nivel-de-dificuldade">
+                    Nível de dificuldade
+                  </Checkbox>
+                  <Checkbox value="ano">Ano</Checkbox>
+                  <Checkbox value="codigo">Código</Checkbox>
+                  <Checkbox value="ordem">Ordem</Checkbox>
+                  <Checkbox value="descritor">Descritor</Checkbox>
+                </CheckboxGroup>
+              </div>
+            </motion.section>
+          </main>
+        </AnimatePresence>
+      </section>
+      <section className="h-full w-full rounded-md bg-white p-3 px-4">
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ ease: "easeInOut", duration: 0.7 }}
+          className="p-6 pt-4 border border-green-400 rounded-md shadow"
         >
-          Mostrar Enunciado
-        </button>
-      </div>
+          <Contexto></Contexto>
+        </motion.section>
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ ease: "easeInOut", duration: 0.7 }}
+          className="p-6 pt-4 border border-blue-400 rounded-md shadow"
+        ></motion.section>
+        <motion.section
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ ease: "easeInOut", duration: 0.7 }}
+          className="p-6 pt-4 border border-fuchsia-400 rounded-md shadow"
+        ></motion.section>
+      </section>
     </div>
   );
 }
