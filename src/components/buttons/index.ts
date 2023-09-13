ed8dc2fe08af924cdeb59f66523902f01b0745b7
Juan Volpi <juan8volpi@gmail.com>
2023-09-13 17:33:34 +0100

    feat: easy import

---
+++ src/components/buttons/index.ts
@@ -0,0 +1,8 @@
+import SmallDeleteButton from '@/components/buttons/SmallDeleteButton';
+import SmallSelectAllButton from "@/components/buttons/SmallSelectAllButton";
+
+export {
+  SmallDeleteButton,
+  SmallSelectAllButton,
+};
+
