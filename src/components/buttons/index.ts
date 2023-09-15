f1cb4cb4278a27b8894871d9378810d221b64986
Juan Volpi <juan8volpi@gmail.com>
2023-09-15 11:11:33 +0100

    ci: adição dos componentes

--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,8 +1,13 @@
 import SmallDeleteButton from '@/components/buttons/SmallDeleteButton';
 import SmallSelectAllButton from "@/components/buttons/SmallSelectAllButton";
+import SmallChevronUpButton from "@/components/buttons/SmallChevronUpButton";
+import SmallChevronDownButton from "@/components/buttons/SmallChevronDownButton";
+
 
 export {
   SmallDeleteButton,
   SmallSelectAllButton,
+  SmallChevronUpButton,
+  SmallChevronDownButton,
 };
 
