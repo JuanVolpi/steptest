72f6cdf0e3e034abdefeadc5126eb706dd03b19d
Juan Volpi <juan8volpi@gmail.com>
2023-10-04 15:00:43 +0100

    feat: nova página apenas para desenvolvimento (sandbox)

---
+++ src/app/dev/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import AveragesTable from '@/components/tables/AveragesTable';
+
+export default function Questoes() {
+  return (
+    <>
+      <div>
+        <AveragesTable></AveragesTable>
+      </div>
+    </>
+  );
+}
