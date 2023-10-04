55d8b81a2cb6e465738cce39f930ed792c6ef472
Juan Volpi <juan8volpi@gmail.com>
2023-10-04 14:59:10 +0100

    style: mudança das cores de background das notas

--- src/components/cards/Ranking.tsx
+++ src/components/cards/Ranking.tsx
@@ -19,10 +19,10 @@ export default function Ranking() {
     switch (placement) {
       case 1:
         return (
-          <div class="quiz-medal">
-            <div class="relative">
-              <div class="w-14 h-14 rounded-full bg-gradient-to-tl border-amber-400 border-3 from-amber-400 via-yellow-100 to-yellow-400 shadow-md flex items-center justify-center text-white font-semibold text-">
-                <span class="relative">{placement}</span>
+          <div className="quiz-medal">
+            <div className="relative">
+              <div className="w-14 h-14 rounded-full bg-gradient-to-tl border-amber-400 border-3 from-amber-400 via-yellow-100 to-yellow-400 shadow-md flex items-center justify-center text-white font-semibold text-">
+                <span className="relative">{placement}</span>
               </div>
             </div>
           </div>
@@ -30,10 +30,10 @@ export default function Ranking() {
         break;
       case 2:
         return (
-          <div class="quiz-medal">
-            <div class="relative">
-              <div class="w-14 h-14 rounded-full  border-gray-400 border-3 bg-gradient-to-tl from-gray-500 via-neutral-100 to-slate-300 shadow-md flex items-center justify-center text-white font-semibold text-">
-                <span class="relative">{placement}</span>
+          <div className="quiz-medal">
+            <div className="relative">
+              <div className="w-14 h-14 rounded-full  border-gray-400 border-3 bg-gradient-to-tl from-gray-500 via-neutral-100 to-slate-300 shadow-md flex items-center justify-center text-white font-semibold text-">
+                <span className="relative">{placement}</span>
               </div>
             </div>
           </div>
@@ -41,10 +41,10 @@ export default function Ranking() {
         break;
       case 3:
         return (
-          <div class="quiz-medal">
-            <div class="relative">
-              <div class="w-14 h-14 rounded-full  border-orange-400 border-3 bg-gradient-to-tl from-orange-800 via-orange-300 to-orange-500 shadow-md flex items-center justify-center text-white font-semibold text-">
-                <span class="relative">{placement}</span>
+          <div className="quiz-medal">
+            <div className="relative">
+              <div className="w-14 h-14 rounded-full  border-orange-400 border-3 bg-gradient-to-tl from-orange-800 via-orange-300 to-orange-500 shadow-md flex items-center justify-center text-white font-semibold text-">
+                <span className="relative">{placement}</span>
               </div>
             </div>
           </div>
@@ -52,10 +52,10 @@ export default function Ranking() {
         break;
       default:
         return (
-          <div class="quiz-medal">
-            <div class="relative">
-              <div class="w-14 h-14 rounded-full  border-sky-400 border-3 bg-gradient-to-tl from-sky-600 via-sky-100 to-sky-300 flex items-center justify-center text-white font-semibold text-">
-                <span class="relative">{placement}</span>
+          <div className="quiz-medal">
+            <div className="relative">
+              <div className="w-14 h-14 rounded-full  border-sky-400 border-3 bg-gradient-to-tl from-sky-600 via-sky-100 to-sky-300 flex items-center justify-center text-white font-semibold text-">
+                <span className="relative">{placement}</span>
               </div>
             </div>
           </div>
@@ -68,7 +68,7 @@ export default function Ranking() {
     <div className="space-y-4 h-96 overflow-scroll">
       {rankList.map((rank, index) => {
         return (
-          <div className="flex flex-row gap-9 w-max h-fit text-3xl rounded-full hover:bg-fuchsia-200 transition-all ease-in-out duration-200">
+          <div className="flex flex-row gap-9 w-max h-fit text-3xl rounded-full hover:bg-sky-200 transition-all ease-in-out duration-200">
             <section className="flex flex-row gap-3">
               <div className="w-max h-full border-b-3 rounded-full p-3 shadow align-middle">
                 {rankingMedals(index + 1)}
@@ -76,7 +76,7 @@ export default function Ranking() {
               <div className="w-max h-full border-b-3 rounded-lg p-3 shadow inline-flex items-center">
                 Turma: {rank.ano}
               </div>
-              <div className="w-16 h-full border-b-3 rounded-lg p-3 shadow font-bold text-fuchsia-600 inline-flex items-center justify-center bg-fuchsia-100">
+              <div className="w-16 h-full border-b-3 rounded-lg p-3 shadow font-bold text-slate-600 inline-flex items-center justify-center bg-neutral-100">
                 {rank.notaMedia}
               </div>
             </section>
